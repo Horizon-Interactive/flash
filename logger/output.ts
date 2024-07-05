daf023220cb244db778225d12c31c75a6e4cc919
Obsidiwayv <pizza5803@gmail.com>
2024-07-05 16:49:50 -0400

    more methods

--- logger/output.ts
+++ logger/output.ts
@@ -1,7 +1,25 @@
 import colors from "./colors";
 
 export default class {
-    public static write(color: string, text: string) {
-        console.log(`${color}${text}${colors.reset}`);
+    public static write(color: string, text: string | string[]) {
+        console.log(
+            `${color}${Array.isArray(text) ? text.join(" ") : text}${colors.reset}`
+        );
+    }
+
+    public static signal(text: string | string[]) {
+        return this.write(colors.fg.green, text);
+    }
+
+    public static alert(text: string | string[]) {
+        return this.write(colors.fg.red, text);
+    }
+
+    public static record(text: string | string[]) {
+        return this.write(colors.fg.cyan, text);
+    }
+
+    public static json(obj: object) {
+        console.info(obj);
     }
 }
